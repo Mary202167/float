b7de9bfebd6fe0be53dea676eaec169f86bbadaa
Mary202167 <85348570+Mary202167@users.noreply.github.com>
2021-07-09 13:19:03 +0300

    Add files via upload

---
+++ NewFile.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main ()
+{
+/*local variable defination*/
+int a=10;
+/*check the boolean condition using if statement*/
+if (a>20);
+{
+	printf("a is greater than 20\n");
+}
+printf("value of a is : %d\n,");
+return 0;
+}
