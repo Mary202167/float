341c92d06d947c7b0b316efecbda92d9042f30e7
Mary202167 <85348570+Mary202167@users.noreply.github.com>
2021-07-09 13:16:24 +0300

    Add files via upload

---
+++ DIVISIBLE BY 17.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main ()
+{
+	int a;
+	printf ("enter the value of a");
+	scanf("%d",&a);
+	if(a%17==0)
+	{
+		printf ("a is divisible by 17");
+	}
+	else
+	{
+      printf("a is not divisible by 17");
+	}
+	
+}
